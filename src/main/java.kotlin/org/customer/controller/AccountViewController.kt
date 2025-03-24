fe63d6be679dedc289e5a6755d222323a60081e2
Cemal Serter <cemalemre88@gmail.com>
2025-03-24 05:32:31 +0300

    view controllers

--- src/main/java.kotlin/org/customer/controller/AccountViewController.kt
+++ src/main/java.kotlin/org/customer/controller/AccountViewController.kt
@@ -1,15 +1,14 @@
-package org.customer.controller;
+package org.customer.controller
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
 
 @Controller
 @RequestMapping("/accounts")
-public class AccountViewController {
-
+class AccountViewController {
     @GetMapping("/open")
-    public String openAccountForm() {
-        return "accounts"; // Refers to accounts.html in /templates/
+    fun openAccountForm(): String {
+        return "accounts" // Refers to accounts.html in /templates/
     }
 }
--- src/main/java.kotlin/org/customer/controller/CustomerViewController.kt
+++ src/main/java.kotlin/org/customer/controller/CustomerViewController.kt
@@ -1,16 +1,15 @@
-package org.customer.controller;
+package org.customer.controller
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
 
 @Controller
 @RequestMapping("/customers")
-public class CustomerViewController {
-
+class CustomerViewController {
     @GetMapping("/details")
-    public String customerDetailsPage() {
-        return "customers"; // Refers to customers.html
+    fun customerDetailsPage(): String {
+        return "customers" // Refers to customers.html
     }
 }
 
